1dbfdaa0f066ddaba8488f448bfdc135c646af06
Ondřej Kuba <xkuba2@mendelu.cz>
2025-12-17 10:50:40 +0100

    qtFiles
    soubory pro qtDeveloper

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appcppProjekt2025Qt_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appcppProjekt2025Qt")
+set(working_dir "C:/Users/kubao/Documents/cppProjekt2025Qt")
+set(src_and_dest_list
+
+)
+set(timestamp_file "C:/Users/kubao/Documents/cppProjekt2025Qt/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appcppProjekt2025Qt_res.txt")
