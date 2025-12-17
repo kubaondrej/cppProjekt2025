b2a63b5c6ef4a37266f2a4f3e43a910dd6df6ca0
Ondřej Kuba <xkuba2@mendelu.cz>
2025-12-17 18:09:13 +0100

    oprava

---
+++ MapModel.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QAbstractListModel>
+#include <vector>
+
+enum class Terrain : int {
+    Grass = 0,
+    Water = 1,
+    Mountain = 2
+};
+
+struct Tile {
+    Terrain terrain = Terrain::Grass;
+};
+
+class MapModel : public QAbstractListModel {
+    Q_OBJECT
+    Q_PROPERTY(int size READ size NOTIFY sizeChanged)
+
+public:
+    enum Roles {
+        TerrainRole = Qt::UserRole + 1
+    };
+
+    explicit MapModel(QObject* parent = nullptr);
+
+    int size() const;
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE void generate(int n);
+
+signals:
+    void sizeChanged();
+
+private:
+    int m_size = 0;
+    std::vector<Tile> m_tiles;
+};
