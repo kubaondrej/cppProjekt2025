bfe0b47c8648013448c13dfaeb70617b51aef50d
Jonas Garec <garec.jonas@protonmail.com>
2025-12-17 17:44:00 +0100

    add gitignore build

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appCPPProjekt_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appCPPProjekt")
-set(working_dir "C:/Users/Micha/OneDrive/Dokumenty/CPPProjekt")
-set(src_and_dest_list
-    "C:/Users/Micha/OneDrive/Dokumenty/CPPProjekt/Main.qml"
-    "C:/Users/Micha/OneDrive/Dokumenty/CPPProjekt/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/CPPProjekt/Main.qml"
-
-)
-set(timestamp_file "C:/Users/Micha/OneDrive/Dokumenty/CPPProjekt/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appCPPProjekt_qml.txt")
