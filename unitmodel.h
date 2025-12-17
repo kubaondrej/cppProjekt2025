00c8057240e107a3d067074ed121ee596ca03cff
Jakub Uhlir <xuhlir@mendelu.cz>
2025-12-17 14:12:35 +0100

    UnitModel.h skeleton

--- unitmodel.h
+++ unitmodel.h
@@ -1,10 +1,42 @@
 #ifndef UNITMODEL_H
 #define UNITMODEL_H
 
-class unitmodel
-{
+#pragma once
+
+#include <QAbstractListModel>
+#include <vector>
+
+enum class UnitType : int {
+    Soldier = 0
+};
+
+struct Unit {
+    int x = 0;
+    int y = 0;
+    UnitType type = UnitType::Soldier;
+};
+
+class UnitModel : public QAbstractListModel {
+    Q_OBJECT
+
 public:
-    unitmodel();
+    enum Roles {
+        XRole = Qt::UserRole + 1,
+        YRole,
+        TypeRole
+    };
+    Q_ENUM(Roles)
+
+    explicit UnitModel(QObject* parent = nullptr);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE void addUnit(int x, int y);
+
+private:
+    std::vector<Unit> m_units;
 };
 
 #endif // UNITMODEL_H
