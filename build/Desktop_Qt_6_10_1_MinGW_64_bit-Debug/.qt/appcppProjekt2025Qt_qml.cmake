e4ac8b763f6433fd66bb756a9f4d8e605e38db21
Ondřej Kuba <xkuba2@mendelu.cz>
2025-12-17 11:12:18 +0100

    odstraneni qt souboru

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appcppProjekt2025Qt_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appcppProjekt2025Qt")
-set(working_dir "C:/Users/kubao/Documents/cppProjekt2025Qt")
-set(src_and_dest_list
-    "C:/Users/kubao/Documents/cppProjekt2025Qt/Main.qml"
-    "C:/Users/kubao/Documents/cppProjekt2025Qt/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/cppProjekt2025Qt/Main.qml"
-
-)
-set(timestamp_file "C:/Users/kubao/Documents/cppProjekt2025Qt/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appcppProjekt2025Qt_qml.txt")
